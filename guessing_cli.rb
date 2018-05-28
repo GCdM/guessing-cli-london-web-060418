17de12de2f4f073bf1cdc4aab060061c45d9e625
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-28 17:21:12 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,13 +1,9 @@
-# Code your solution here!
-require 'pry'
-
 def run_guessing_game
   loop do
     puts "Guess a number between 1 and 6."
     input = gets.chomp
     innum = input.to_i
     rnum = rand(6)
-    binding.pry
     if input == "exit"
       puts "Goodbye!"
       break
